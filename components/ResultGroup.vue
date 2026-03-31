534c1def57fa6e150768460b4b0dad8d85988926
markz_demo <markz_demo@163.com>
2026-03-31 02:27:17 +0000

    1

--- components/ResultGroup.vue
+++ components/ResultGroup.vue
@@ -50,6 +50,17 @@
                 {{ formatDate(r.datetime) || "时间未知" }}
               </span>
 
+              <span class="meta-tag date">
+                <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                  <line x1="16" y1="2" x2="16" y2="6"></line>
+                  <line x1="8" y1="2" x2="8" y2="6"></line>
+                  <line x1="3" y1="10" x2="21" y2="10"></line>
+                </svg>
+                {{ (r.source) || "none" }}
+              </span>
+
+
               <span v-if="r.password" class="meta-tag password">
                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                   <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
@@ -266,7 +277,7 @@ function formatDate(d?: string) {
 .resource-content {
   display: flex;
   flex-direction: column;
-  gap: 8px;
+  gap: 4px;
 }
 
 /* 资源链接 */
